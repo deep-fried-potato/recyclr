c92fa1bdbdfae6352bbb8da1c03efd09eef83879
Sri Sailesh Meegada <36806193+deep-fried-potato@users.noreply.github.com>
2020-03-05 18:55:08 +0530

    added device schema

---
+++ models/device.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.Types.ObjectId
+var deviceSchema = new Schema({
+  name: String,
+  description:String,
+  manufacturer:String,
+  parts:[{
+    type:ObjectId,
+    ref:'part'
+  }]
+});
+module.exports = mongoose.model('device',deviceSchema)
